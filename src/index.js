c3051dd35d77ca3c861b7c380eaab70d411ecf57
Andreea Furtuna <furtuna.andreeac@gmail.com>
2020-07-16 00:00:56 +0200

    Updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -8,6 +34,7 @@ function displayTemperature(response) {
   let feelElement = document.querySelector("#feel");
   let maxElement = document.querySelector("#max");
   let minElement = document.querySelector("#min");
+  let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
@@ -17,9 +44,10 @@ function displayTemperature(response) {
   feelElement.innerHTML = Math.round(response.data.main.feels_like);
   maxElement.innerHTML = Math.round(response.data.main.temp_max);
   minElement.innerHTML = Math.round(response.data.main.temp_min);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "6ef1e8464c3d2cf1f0b480456f49e6c9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
